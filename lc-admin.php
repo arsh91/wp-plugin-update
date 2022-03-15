5c2d78709696640694e1bb701585539a4a5dd253
arsh91 <arshsharma91@gmail.com>
2022-03-15 13:36:53 +0530

    Plugin version update

--- lc-admin.php
+++ lc-admin.php
@@ -1,15 +1,9 @@
 <?php
 /**
  * Plugin Name:      LC Admin update
- * @link              https://www.leadconnectorhq.com/
- * @since             1.0
- * @package           Leadconnector_admin
- *Plugin URI: https://github.com/simranjit-developer/git-first.git
- * @wordpress-plugin
- * Plugin Name:       LC Admin by LeadConnector x
  * Plugin URI:        https://www.leadconnectorhq.com/
  * Description:       Users of the API can authenticate with genterated token-id and a location-id.
- * Version:           1.0
+ * Version:           1.1
  * Author:            LeadConnector
  * Author URI:        https://www.leadconnectorhq.com/
  * License:           GPL-2.0+
